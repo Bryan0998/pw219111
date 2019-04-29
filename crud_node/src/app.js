fb4a8560e5bf5c5b6849446494cf2271ce568e67
Bryan0998 <13170274@itculiacan.edu.mx>
2019-04-29 12:05:47 -0600

    Conexion a la base de datos

--- crud_node/src/app.js
+++ crud_node/src/app.js
@@ -1,10 +1,31 @@
+const path = require('path');
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const myConnection = require('express-myconnection');
+const mysql = require('mysql');
+
 //importar rutas
 const indiceRutas=require('./rutas/index');
 
 //usamos las rutas
 app.use('/',indiceRutas);
+
+//configuraciones
+app.set('port',process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'views'));
+
+//MiddleWare (Morgan)
+app.use(morgan('dev')); //dev = desarrollo
+app.use(myConnection(mysql,
+{
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	port: 3306,
+	database: 'crudnodejsmysql'
+}),'single')
 
 //Activar el servidor de escucha
 app.listen(3000, () => {
